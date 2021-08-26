dd8566e5cf61f45c9393b1649ed78c02664a0e0e
Ally Nicolella <31012168+aanicolella@users.noreply.github.com>
2021-08-26 10:45:37 -0400

    Implement a more robust approach

--- celltypePseudobulkDE.R
+++ celltypePseudobulkDE.R
@@ -1,34 +1,144 @@
 library("DESeq2")
-filepath <- "parentDir/cellType_totalExpression_genoWT_DATE.csv"
 
+# Variables to replace
+    # cellType: the name of the cell type you wish to analyze
+    # X, Y, Z: number of replicates for HT, KO, WT
+
+# Load cellType count sheet for cellType
+filepath <- "parentDir/cellType_totalExpression_date.csv"
 rsem.in <- as.matrix(read.csv(filepath, row.names=1))
 head(rsem.in)
-colData = data.frame(row.names=colnames(rsem.in),model=c(rep(c("GENE_geno"), 4), rep(c("GENE_WT"), 4)),sample=colnames(rsem.in))
-colData$model = relevel(colData$model, "GENE_WT")
 
+# Set up metadata
+    # model = genotype 
+    # sample = individual sample name
+colData = data.frame(model=c(rep(c("EXP_HT"), X), rep(c("EXP_KO"), Y), rep(c("EXP_WT"), Z)),sample=colnames(rsem.in))
+colData$model = relevel(colData$model, "EXP_WT")
+
+# Optional: find genes below expression threshhold. Create 'NA' data frame to re-add lowly expressed 
+# genes for easier comparison between analyses.
+drop <- rsem.in
+drop$abundance <-
+    drop$abundance[apply(drop$length,
+                             1,
+                             function(row) !all(row > 5)),]                              
+drop$counts <-
+  drop$counts[apply(drop$length,
+                            1,
+                             function(row) !all(row > 5)),]
+drop$length <-
+  drop$length[apply(drop$length,
+                             1,
+                             function(row) !all(row > 5)),]
+                    
+dropped <- drop$counts
+dropRes <- data.frame(row.names=make.unique(row.names(drop$counts)), matrix(ncol=6, nrow=dim(drop$counts)[1]))
+cols = c("baseMean", "log2FoldChange", "lfcSE", "stat", "pvalue", "padj")
+colnames(dropRes) <- cols
+EXPName1 <- data.frame(do.call('rbind', strsplit(as.character(row.names(dropRes)), '_', fixed=TRUE)))
+EXPName1 <- EXPName1["X2"]               
+dropRes$symbol <- EXPName1$X2
+
+# Filter lowly expressed genes found above out of the main dataset for the DE analysis
+rsem.in$abundance <-
+  rsem.in$abundance[apply(rsem.in$length,
+                             1,
+                             function(row) all(row > 5 )),]
+rsem.in$counts <-
+  rsem.in$counts[apply(rsem.in$length,
+                             1,
+                             function(row) all(row > 5 )),]
+rsem.in$length <-
+  rsem.in$length[apply(rsem.in$length,
+                             1,
+                             function(row) all(row > 5 )),]
+
+# Convert data structure to proper format and run DESeq
 dds <- DESeqDataSetFromMatrix(countData = rsem.in,
                               colData = colData,
                               design = ~model)
-dds
 dds <- DESeq(dds)
+check <- estimateSizeFactors(dds)
+norm <- counts(check, normalized=TRUE)
+                       
+# Combine normalized count dataframe with the 'NA' dataframe and save
+all <- rbind(norm, dropped)                                          
+EXPName <- data.frame(do.call('rbind', strsplit(as.character(row.names(all)), '_', fixed=TRUE)))
+EXPName <- EXPName["X2"]               
+row.names(all) <- EXPName$X2
+all <- all[order(row.names(all)),]
+head(all)
+write.csv(all, "cellType_normalizedCounts.csv", row.names=TRUE)
+                       
+# Plot pca using different metadata conditions to see relationships between samples
+pdf(file="cellType_pca.pdf")
+vsdata <- vst(dds, blind=FALSE)
+plotPCA(vsdata, intgroup="sample")
+dev.off()
+pdf(file="cellType_modelpca.pdf")
+vsdata <- vst(dds, blind=FALSE)
+plotPCA(vsdata, intgroup="model")
+dev.off()
 
-res <- results(dds)
-head(res,6)
+# Extract DE results from different genotype comparisions
+    # Currently set up such that the LFC directionality is in relation to the changes in the first 
+    # genotype listed: i.e. + lfc == upregulation in that geno, - lfc == downregulation
+    # KOHT relationship is optional, sometimes interesting, primarily for QC purposes
+resHT <- results(dds, contrast=c("model", "EXP_HT","EXP_WT"))
+resKO <- results(dds, contrast=c("model", "EXP_KO","EXP_WT"))
+resKOHT <- results(dds, contrast=c("model", "EXP_KO","EXP_HT"))
+head(results(dds, tidy=TRUE), n=6)
 summary(dds)
 
-res <- lfcShrink(dds=dds, coef=2, res=res, type="normal")
-res <- res[order(res$pvalue),]
-head(res, 6)
+# Combine unscaled HTvsWT results and 'NA' dropped gene dataframe then write to .csv
+EXPNameHT <- data.frame(do.call('rbind', strsplit(as.character(row.names(resHT)), '_', fixed=TRUE)))
+EXPNameHT <- EXPNameHT["X2"]               
+resHT$symbol <- EXPNameHT$X2
+resMergeHT <- rbind(resHT, dropRes)
+dim(resMergeHT)
+resMergeHT <- resMergeHT[order(resMergeHT$pvalue),]
+write.csv(resMergeHT, "cellType_HTWT-DESeq2_allgenes.csv")
+# Scale HTvsWT results using lfcShrink(): https://rdrr.io/bioc/DESeq2/man/lfcShrink.html
+   # Combine post-shrinkage results with 'NA' dropped dataframe then write to .csv
+resHT <- lfcShrink(dds=dds, contrast=c("model", "EXP_HT", "EXP_WT"), res=resHT, type="normal")
+EXPNameHT <- data.frame(do.call('rbind', strsplit(as.character(row.names(resHT)), '_', fixed=TRUE)))
+EXPNameHT <- EXPNameHT["X2"]               
+resHT$symbol <- EXPNameHT$X2
+resMergeHT <- rbind(resHT, dropRes)
+dim(resMergeHT)
+resMergeHT <- resMergeHT[order(resMergeHT$pvalue),]
+write.csv(resMergeHT, "cellType_HTWT-DESeq2_LFCshrink.csv")
 
-pdf(file="GENE_PROJECT_cellType_genoWT_volcano_DATE.pdf")              
-high <- row.names(res)[1:10]
-high
-                         
-par(mfrow=c(1,1))
-with(res, plot(log2FoldChange, -log10(pvalue), pch=20, main="GENE_PROJECT_cellType_genoWT"))
-with(subset(res, padj<0.05), points(log2FoldChange, -log10(pvalue), pch=20, col="blue"))
-with(subset(res, row.names(res) %in% high), text(log2FoldChange, -log10(pvalue), labels=high, cex=0.7, font=1, col="red"))
-dev.off()                   
+# Repeat above for KOvsWT
+EXPNameKO <- data.frame(do.call('rbind', strsplit(as.character(row.names(resKO)), '_', fixed=TRUE)))
+EXPNameKO <- EXPNameKO["X2"]               
+resKO$symbol <- EXPNameKO$X2
+resMergeKO <- rbind(resKO, dropRes)
+dim(resMergeKO)
+resMergeKO <- resMergeKO[order(resMergeKO$pvalue),]
+write.csv(resMergeKO, "cellType_KOWT-DESeq2_allgenes.csv")
+resKO <- lfcShrink(dds=dds, contrast=c("model", "EXP_KO", "EXP_WT"), res=resKO, type="normal")
+EXPNameKO <- data.frame(do.call('rbind', strsplit(as.character(row.names(resKO)), '_', fixed=TRUE)))
+EXPNameKO <- EXPNameKO["X2"]               
+resKO$symbol <- EXPNameKO$X2
+resMergeKO <- rbind(resKO, dropRes)
+dim(resMergeKO)
+resMergeKO <- resMergeKO[order(resMergeKO$pvalue),]
+write.csv(resMergeKO, "cellType_KOWT-DESeq2_LFCshrink.csv")
 
-res["GENE",]
-write.csv(res, "GENE_PROJECT_cellType_genoWT-DESeq2_DATE.csv")
+# Repeat above for KOvsHT
+EXPNameKOHT <- data.frame(do.call('rbind', strsplit(as.character(row.names(resKOHT)), '_', fixed=TRUE)))
+EXPNameKOHT <- EXPNameKOHT["X2"]               
+resKOHT$symbol <- EXPNameKOHT$X2
+resMergeKOHT <- rbind(resKOHT, dropRes)
+dim(resMergeKOHT)
+resMergeKOHT <- resMergeKOHT[order(resMergeKOHT$pvalue),]
+write.csv(resMergeKOHT, "cellType_KOHTWT-DESeq2_allgenes.csv")
+resKOHT <- lfcShrink(dds=dds, contrast=c("model", "EXP_KO", "EXP_HT"), res=resKOHT, type="normal")
+EXPNameKOHT <- data.frame(do.call('rbind', strsplit(as.character(row.names(resKOHT)), '_', fixed=TRUE)))
+EXPNameKOHT <- EXPNameKOHT["X2"]               
+resKOHT$symbol <- EXPNameKOHT$X2
+resMergeKOHT <- rbind(resKOHT, dropRes)
+dim(resMergeKOHT)
+resMergeKOHT <- resMergeKOHT[order(resMergeKOHT$pvalue),]
+write.csv(resMergeKOHT, "cellType_KOHT-DESeq2_LFCshrink.csv")
